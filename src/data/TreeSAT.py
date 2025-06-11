fac68c05b914890757bda08345967d46b10d4f18
blaz.rolih <blaz.rolih@fri.uni-lj.si>
2025-06-11 09:45:16 +0200

    Reorganise configs and files for TSAI

--- src/data/TreeSAT.py
+++ src/data/TreeSAT.py
@@ -94,7 +94,7 @@ class TreeSAT(Dataset):
             partition (float): proportion of the dataset to keep
             mono_strict (bool): if True, puts monodate in same condition as multitemporal
         """
-        self.path = "/home/filip/OmniSat/data/TreeSat/"
+        self.path = path
         self.transform = transform
         self.partition = partition
         self.modalities = modalities
@@ -139,65 +139,65 @@ class TreeSAT(Dataset):
             with rasterio.open(self.path + "aerial/" + name) as f:
                 output["aerial"] = torch.FloatTensor(f.read())
 
-        with h5py.File(
-            self.path + "sentinel/" + ".".join(name.split(".")[:-1]) + ".h5", "r"
-        ) as file:
-            if "s1-asc" in self.modalities:
-                output["s1-asc_dates"] = day_number_in_year(
-                    file["sen-1-asc-products"][:]
-                )
-            if "s1-des" in self.modalities:
-                output["s1-des_dates"] = day_number_in_year(
-                    file["sen-1-des-products"][:]
-                )
-            if "s1" in self.modalities:
-                s1_asc_dates = day_number_in_year(file["sen-1-asc-products"][:])
-                s1_des_dates = day_number_in_year(file["sen-1-des-products"][:])
-            if "s2" in self.modalities:
-                output["s2"] = torch.tensor(file["sen-2-data"][:])
-                output["s2_dates"] = day_number_in_year(
-                    file["sen-2-products"][:], place=2
-                )
-                N = len(output["s2_dates"])
-                if N > 50:
-                    random_indices = torch.randperm(N)[:50]
-                    output["s2"] = output["s2"][random_indices]
-                    output["s2_dates"] = output["s2_dates"][random_indices]
+        # with h5py.File(
+        #     self.path + "sentinel/" + ".".join(name.split(".")[:-1]) + ".h5", "r"
+        # ) as file:
+        #     if "s1-asc" in self.modalities:
+        #         output["s1-asc_dates"] = day_number_in_year(
+        #             file["sen-1-asc-products"][:]
+        #         )
+        #     if "s1-des" in self.modalities:
+        #         output["s1-des_dates"] = day_number_in_year(
+        #             file["sen-1-des-products"][:]
+        #         )
+        #     if "s1" in self.modalities:
+        #         s1_asc_dates = day_number_in_year(file["sen-1-asc-products"][:])
+        #         s1_des_dates = day_number_in_year(file["sen-1-des-products"][:])
+        #     if "s2" in self.modalities:
+        #         output["s2"] = torch.tensor(file["sen-2-data"][:])
+        #         output["s2_dates"] = day_number_in_year(
+        #             file["sen-2-products"][:], place=2
+        #         )
+        #         N = len(output["s2_dates"])
+        #         if N > 50:
+        #             random_indices = torch.randperm(N)[:50]
+        #             output["s2"] = output["s2"][random_indices]
+        #             output["s2_dates"] = output["s2_dates"][random_indices]
 
-        if "s1-asc" in self.modalities:
-            output["s1-asc"] = torch.load(
-                self.path + "s1-asc/" + ".".join(name.split(".")[:-1]) + ".pth"
-            )[:, :2, :, :]
-            N = len(output["s1-asc_dates"])
-            if N > 50:
-                random_indices = torch.randperm(N)[:50]
-                output["s1-asc"] = output["s1-asc"][random_indices]
-                output["s1-asc_dates"] = output["s1-asc_dates"][random_indices]
-
-        if "s1-des" in self.modalities:
-            output["s1-des"] = torch.load(
-                self.path + "s1-des/" + ".".join(name.split(".")[:-1]) + ".pth"
-            )[:, :2, :, :]
-            N = len(output["s1-des_dates"])
-            if N > 50:
-                random_indices = torch.randperm(N)[:50]
-                output["s1-des"] = output["s1-des"][random_indices]
-                output["s1-des_dates"] = output["s1-des_dates"][random_indices]
-
-        if "s1" in self.modalities:
-            s1_asc = torch.load(
-                self.path + "s1-asc/" + ".".join(name.split(".")[:-1]) + ".pth"
-            )[:, :2, :, :]
-            s1_des = torch.load(
-                self.path + "s1-des/" + ".".join(name.split(".")[:-1]) + ".pth"
-            )[:, :2, :, :]
-            output["s1"] = torch.cat([s1_asc, s1_des], dim=0)
-            output["s1_dates"] = torch.cat([s1_asc_dates, s1_des_dates], dim=0)
-            N = len(output["s1_dates"])
-            if N > 50:
-                random_indices = torch.randperm(N)[:50]
-                output["s1"] = output["s1"][random_indices]
-                output["s1_dates"] = output["s1_dates"][random_indices]
+        # if "s1-asc" in self.modalities:
+        #     output["s1-asc"] = torch.load(
+        #         self.path + "s1-asc/" + ".".join(name.split(".")[:-1]) + ".pth"
+        #     )[:, :2, :, :]
+        #     N = len(output["s1-asc_dates"])
+        #     if N > 50:
+        #         random_indices = torch.randperm(N)[:50]
+        #         output["s1-asc"] = output["s1-asc"][random_indices]
+        #         output["s1-asc_dates"] = output["s1-asc_dates"][random_indices]
+        #
+        # if "s1-des" in self.modalities:
+        #     output["s1-des"] = torch.load(
+        #         self.path + "s1-des/" + ".".join(name.split(".")[:-1]) + ".pth"
+        #     )[:, :2, :, :]
+        #     N = len(output["s1-des_dates"])
+        #     if N > 50:
+        #         random_indices = torch.randperm(N)[:50]
+        #         output["s1-des"] = output["s1-des"][random_indices]
+        #         output["s1-des_dates"] = output["s1-des_dates"][random_indices]
+        #
+        # if "s1" in self.modalities:
+        #     s1_asc = torch.load(
+        #         self.path + "s1-asc/" + ".".join(name.split(".")[:-1]) + ".pth"
+        #     )[:, :2, :, :]
+        #     s1_des = torch.load(
+        #         self.path + "s1-des/" + ".".join(name.split(".")[:-1]) + ".pth"
+        #     )[:, :2, :, :]
+        #     output["s1"] = torch.cat([s1_asc, s1_des], dim=0)
+        #     output["s1_dates"] = torch.cat([s1_asc_dates, s1_des_dates], dim=0)
+        #     N = len(output["s1_dates"])
+        #     if N > 50:
+        #         random_indices = torch.randperm(N)[:50]
+        #         output["s1"] = output["s1"][random_indices]
+        #         output["s1_dates"] = output["s1_dates"][random_indices]
 
         if "s1-mono" in self.modalities:
             with rasterio.open(self.path + "s1/60m/" + name) as f:
